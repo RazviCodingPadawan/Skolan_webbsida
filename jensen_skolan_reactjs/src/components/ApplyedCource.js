01c0f71e500a333717b3748048cb61ed289170f4
Eijey11 <arturs.jarovojs2018@gmail.com>
2022-03-29 19:21:52 +0200

    New apply commit

--- jensen_skolan_reactjs/src/components/ApplyedCource.js
+++ jensen_skolan_reactjs/src/components/ApplyedCource.js
@@ -1,3 +1,5 @@
+import "../pages/Apply"
+
 
 const ApplyedCource = () => {
 
--- jensen_skolan_reactjs/src/pages/Apply.js
+++ jensen_skolan_reactjs/src/pages/Apply.js
@@ -4,22 +4,18 @@ import { ApplyedCource } from '../components/ApplyedCource'
 
 const Apply = () => {
 
-    const createdCourse = ['Course 1', 'Course 2', 'Course 3', 'Course 4', 'Course 5']
-    const newItem = createdCourse.map((createdCourse) => <ul>{createdCourse}</ul>);
-
     return (
         <div className = "container">
             <div className='apply_container'>
                 <form className='form_apply' action="/action.php">
                     <div className='form_input'>
                         <label for="utbildning">Välja utbildning:</label>
-                        <input list="browsers" name="browser"/>
+                        <input list="browsers" name="browsers"/>
                         <datalist id="browsers">
-                            <option value={newItem}/>
-                            <option value={newItem}/>
-                            <option value={newItem}/>
-                            <option value={newItem}/>
-                            <option value={newItem}/>
+                            <option value= 'Course 1'/>
+                            <option value= 'Course 2'/>
+                            <option value= 'Course 3'/>
+                            <option value= 'Course 4'/>
                         </datalist>
                     </div>
                     <div className='form_input'>
@@ -33,6 +29,10 @@ const Apply = () => {
                     <div className='form_input'>
                         <label for="Epost">E-post:</label>
                         <input type="text" id="email" value="Ditt E-post..."/>
+                    </div>
+                    <div className='form_input'>
+                        <label for="Textarea">Meddelande:</label>
+                        <textarea type="text" id="email" value="Ditt E-post..."/>
                     </div>
                     <button className="btn_submit" type="submit">SKICKA</button>
                 </form>
