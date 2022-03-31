18d86b416bbbc8a5ff00e06e3199d80331e4e471
nataliaermeryd <natalia.ermeryd@outlook.com>
2022-03-31 17:14:50 +0200

    AddingCourse database & api

---
+++ jensen_skolan_reactjs/src/NATTA/routes/api.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+const router = express.Router();
+
+const CourseDb = require("../models/CourseDb");
+
+// Routes
+router.get("/", (req, res) => {
+
+    CourseDb.find({  })
+        .then((data) => {
+            console.log("Data: ", data);
+            res.json(data);
+        })
+        .catch((error) => {
+            console.log("error: ", daerrorta);
+        });
+});
+
+router.post("/save", (req, res) => {
+    const data = req.body;
+
+    const newCourseDb = new CourseDb(data);
+
+    newCourseDb.save((error) => {
+        if (error) {
+            res.status(500).json({ msg: "Sorry, internal server errors" });
+            return;
+        }
+        // CourseDb
+        return res.json({
+            msg: "Your data has been saved!!!!!!"
+        });
+    });
+});
+
+module.exports = router;
